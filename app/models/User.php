67338c58d563096307ad690c33054ec56edbbe1d
tgunawardhana <tgunawardhana@algomau.ca>
2025-06-19 16:15:03 +0000

    Moved user registration code to Register.php model

--- app/models/User.php
+++ app/models/User.php
@@ -18,68 +18,44 @@ class User {
       return $rows;
     }
 
+
+
     public function authenticate($username, $password) {
         /*
          * if username and password good then
          * $this->auth = true;
          */
-		$username = strtolower($username);
-		$db = db_connect();
-        $statement = $db->prepare("select * from users WHERE username = :name;");
-        $statement->bindValue(':name', $username);
-        $statement->execute();
-        $rows = $statement->fetch(PDO::FETCH_ASSOC);
-		
-		if (password_verify($password, $rows['password'])) {
-			$_SESSION['auth'] = 1;
-			$_SESSION['username'] = ucwords($username);
-			unset($_SESSION['failedAuth']);
-			header('Location: /home');
-			die;
-		} else {
-			if(isset($_SESSION['failedAuth'])) {
-				$_SESSION['failedAuth'] ++; //increment
+  		$username = strtolower($username);
+  		$db = db_connect();
+          $statement = $db->prepare("select * from users WHERE username = :name;");
+          $statement->bindValue(':name', $username);
+          $statement->execute();
+          $rows = $statement->fetch(PDO::FETCH_ASSOC);
+  		
+  		if (password_verify($password, $rows['password'])) {
+  			$_SESSION['auth'] = 1;
+  			$_SESSION['username'] = ucwords($username);
+  			unset($_SESSION['failedAuth']);
+    
+  			header('Location: /home');
+  
+  			die;
+  		} else {
+  			if(isset($_SESSION['failedAuth'])) {
+  				$_SESSION['failedAuth'] ++; //increment
+          
+  			} else {
+  				$_SESSION['failedAuth'] = 1;
+  			}
         
-			} else {
-				$_SESSION['failedAuth'] = 1;
-			}
-			header('Location: /login');
-			die;
-		}
+        
+  			header('Location: /login');
+  			die;
+  		}
     }
 
 
-  public function get_user_by_username($username) {
-    $dbh = db_connect();
-    $statement = $dbh->prepare("select * from users where username = :username;");
-    $statement->bindParam(':username', $username);
-    $statement->execute();
-    $row = $statement->fetch(PDO::FETCH_ASSOC);
-    return $row;
-  }
-
-  public function register_user($username, $password) {
-
-    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-    $existing_user_data = $this->get_user_by_username($username);
-
-    if ($existing_user_data && $existing_user_data['username'] == $username)  {
-      $_SESSION['error_signup'] = 4;
-      header("location: /signup");
-      die();
-    }
-    else {
-
-      $dbh = db_connect();
-      $statement = $dbh->prepare("insert into users (username, password) values (:username, :password);");
-      $statement->bindParam(':username', $username);
-      $statement->bindParam(':password', $hashed_password);
-      $statement->execute();
-      $_SESSION['signup_complete'] = 1;
-      header("location: /login");
-      die();
-    }
-  }
+  
   
 
   
